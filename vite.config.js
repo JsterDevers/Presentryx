08e660309ca8e589ddefd84fd2f723ad520b0913
John Michael G. De Vera <johnmichaelgunita2005@gmail.com>
2025-11-24 09:20:56 +0000

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import 'tailwindcss'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: true, // Exposes the server to the cloud environment
+    allowedHosts: true, // Allows the dynamic Codespaces URL
+    hmr: {
+      clientPort: 443 // Fixes the "WebSocket connection failed" error in Codespaces
+    }
+  }
 })
